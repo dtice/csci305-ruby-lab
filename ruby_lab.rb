57181450e379f0509b07e2d063700c2d6fde6400
Dill <dillon.tice@gmail.com>
2018-02-02 18:02:34 -0700

    Split on <SEP> token

--- ruby_lab.rb
+++ ruby_lab.rb
@@ -4,13 +4,13 @@
 #
 # CSCI 305 - Ruby Programming Lab
 #
-# <firstname> <lastname>
-# <email-address>
+# Dillon Tice
+# Dillon.Tice@gmail.com
 #
 ###############################################################
 
 $bigrams = Hash.new # The Bigram data structure
-$name = "<firstname> <lastname>"
+$name = "Dillon Tice"
 
 # function to process each line of a file and extract the song titles
 def process_file(file_name)
@@ -19,6 +19,10 @@ def process_file(file_name)
 	begin
 		IO.foreach(file_name) do |line|
 			# do something for each line
+            # 1. cleanup_title(line)
+            # 2. Ensure there are no non-english characters
+            # 3. Convert all characters to lower case
+            cleanup_title(line)
 		end
 
 		puts "Finished. Bigram model built.\n"
@@ -26,6 +30,14 @@ def process_file(file_name)
 		STDERR.puts "Could not open file"
 		exit 4
 	end
+end
+
+def cleanup_title(dirty_string)
+    # 0. Split on <SEP> *DONE*
+    # 1. Remove all things following (following meaning until you hit a space?) these characters:  (  [  {  \  /  _  -  :  "  `  +  =  *  feat.
+    # 2. Delete the following punctuation marks globally:  ?  ¿  !  ¡  .  ;  &  @  %  #  |
+    p song_title = dirty_string.split('<SEP>', 4).last
+    
 end
 
 # Executes the program
